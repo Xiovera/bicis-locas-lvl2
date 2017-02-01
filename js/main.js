279b1dd4a49e7e21ca6dea07b63f4f8899a206f3
Rocio Yovera <rocioyovera@ gmail.com>
2017-02-01 16:09:42 -0500

    finalizado...por fin

--- js/main.js
+++ js/main.js
@@ -1,21 +1,68 @@
+//funcion general para crear span
+
+function crearSpan(inputID, mensaje)
+{
+    var input = document.getElementById(inputID);
+    var nuevoSpan = document.createElement("span");
+    nuevoSpan.innerHTML= mensaje;
+   
+    if(input.nextElementSibling == null){
+        input.parentNode.appendChild(nuevoSpan);
+    }
+}
+
+//funcion general para eliminar span
+
+function eliminarSpan(inputID)
+{
+    var input = document.getElementById(inputID);   
+    if(input.nextSibling != null){
+        input.parentNode.removeChild(input.nextSibling);
+    }
+}
+
+//validando todo el formulario
+
 function validateForm(){
-	var nodoname = document.createElement("span");
-	nodoname.innerText = "Debe ingresar su nombre";
-	contenedorName.appendChild(nodoname);
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var contraseña= document.getElementById("input-password").value;
+	var lista = document.getElementById("bici").selectedIndex;
 
-	var nodolastname = document.createElement("span");
-	nodolastname.innerText = "Debe ingresar su apellido";
-	contenedorLastName.appendChild(nodolastname);
+	if(nombre===""||apellido===""||email===""||contraseña===""){
+		alert("Por favor, complete los 5 primeros campos");
+		return false;
+	}
+	else if (!(/^[a-zA-Z\s]*$/).test(nombre)){
+		crearSpan("name","Los numeros no estan permitidos")
+	}else{
+		eliminarSpan("name");
+	}
 
-	var nodoemail = document.createElement("span");
-	nodoemail.innerText = "Verifique su email, Ej: name@domain.com.";
-	contenedorEmail.appendChild(nodoemail);
+	if (!(/^[a-zA-Z\s]*$/).test(apellido)){
+		crearSpan("lastname","Los numeros no estan permitidos")
+	}else{
+		eliminarSpan("lastname");
+	}
 
-	var nodopassword = document.createElement("span");
-	nodopassword.innerText = "la contraseña debe tener al menos 6 caracteres";
-	contenedorPassword.appendChild(nodopassword);
+	if (!/\w+@\w+\.+[a-z]/.test(email)){
+		crearSpan("input-email","E-mail invalido, Ej. nombre@dominio.com")
+	}else{
+		eliminarSpan("input-email");
+	}
+	if (contraseña.length < 6 || contraseña === "password"|| contraseña==="123456"|| contraseña==="098754"){
+		crearSpan("input-password","La contraseña ingresada debe tener mas de 6 caracteres")
+	}else{
+		eliminarSpan("input-password");
+	}
+	 if( lista == null || lista == 0 ) {
+        crearSpan("bici", "Elija una opcion de la lista");
+        
+    }else{
+        eliminarSpan("bici");
+    }
 
-	var nodooptions = document.createElement("span");
-	nodooptions.innerText = "Debe seleccionar al menos un tipo de bici";
-	contenedorOptions.appendChild(nodooptions);
+	
+
 }
