46f006226107e6eeb67aa994b525ac20117be5a4
Rocio Yovera <rocioyovera@ gmail.com>
2017-02-24 10:55:20 -0500

    salio

--- js/my-jquery.js
+++ js/my-jquery.js
@@ -1,63 +1,59 @@
-// funcion general para crear span
-function createSpan(inputID, message)
+function crearSpan(inputID, mensaje)
 {
-	var input = $('#inputID').val();
-
-	if(!input.next()){
-		input.parent().append('<span>'+ message +'</span>');
-	}else{
-		if(input.next().is('span'))
-			input.next().html(message);
-	}
-
+    var input = document.getElementById(inputID);
+    var nuevoSpan = document.createElement("span");
+    nuevoSpan.innerHTML= mensaje;
+   
+    if(input.nextElementSibling == null){
+        input.parentNode.appendChild(nuevoSpan);
+    }
 }
 
 //funcion general para eliminar span
-function deleteSpan(inputID)
+
+function eliminarSpan(inputID)
 {
-	var input = $('#inputID').val();
-	if(input.parent().find('span').length>0)
-	{
-		input.parent().find('span').remove();
-	}
-}
+    var input = document.getElementById(inputID);   
+    if(input.nextSibling != null){
+        input.parentNode.removeChild(input.nextSibling);
+    }
 }
 
 //validando todo el formulario
 function validateForm(){
-	var name = $('#name').val();
-	var lastName = $('#lastname').val();
-	var email = $('#input-email').val();
-	var password = $('#input-password').val();
-	var selectBici = $('select').val();
+	//var name = $('#name').val();
+	//var lastName = $('#lastname').val();
+	//var email = $('#input-email').val();
+	//var password = $('#input-password').val();
+	//var selectBici = $('select').val();
 
-	if(name===""||lastName===""||email===""||password==""||selectBici===""||){
+	if($('#name').val()===""||$('#lastname').val()===""||$('#input-email').val()===""||$('#input-password').val()==""||$('select').val()===""||){
 		alert("Por favor, complete los 5 primeros campos");
 		return false;
 	}
-	else if (!(/^[a-zA-Z\s]*$/).test(name)){
+	else if (!(/^[a-zA-Z\s]*$/).test($('#name').val())){
 		createSpan("name", "los numeros no están permitidos")
 	}else{
 		deleteSpan("name");
 	}
 
-	if (!(/^[a-zA-Z\s]*$/).test(apellido)){
+	if (!(/^[a-zA-Z\s]*$/).test($('#lastname').val())){
 		createSpan("lastname","Los numeros no estan permitidos")
 	}else{
 		deleteSpan("lastname");
 	}
 
-	if (!/\w+@\w+\.+[a-z]/.test(email)){
+	if (!/\w+@\w+\.+[a-z]/.test($('#input-email').val())){
 		createSpan("input-email","E-mail invalido, Ej. nombre@dominio.com")
 	}else{
 		deleteSpan("input-email");
 	}
-	if (contraseña.length < 6 || contraseña === "password"|| contraseña==="123456"|| contraseña==="098754"){
+	if ($('#input-password').val().length < 6 || $('#input-password').val() === "password"|| $('#input-password').val()==="123456"|| contraseña==="098754"){
 		createSpan("input-password","La contraseña ingresada debe tener mas de 6 caracteres")
 	}else{
 		deleteSpan("input-password");
 	}
-	 if( lista == null || lista == 0 ) {
+	 if( $('select').val() == null || $('select').val() == 0 ) {
         createSpan("bici", "Elija una opcion de la lista");
         
     }else{
